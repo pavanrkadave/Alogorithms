2d34f15bdda98ee8e52ea37a6791fdd394c83043
Pavan Hegde <pavanrkadave@gmail.com>
2017-12-30 21:03:38 +0530

    Practice Data Structures

--- src/pavanrkadave/github/io/Main.java
+++ src/pavanrkadave/github/io/Main.java
@@ -1,5 +1,6 @@
 package pavanrkadave.github.io;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -11,7 +12,7 @@ public class Main {
     public static String user_Str = "";
     public static ArrayList<String> dict;
 
-    public static char temp_char[];
+    public static String temp_char[];
 
     public static void main(String[] args) {
 
@@ -27,12 +28,20 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         user_Str = sc.nextLine();
         user_Str.trim();
+        temp_char = new String[user_Str.length()];
 
         while (count != user_Str.length()) {
-            System.out.println(user_Str.charAt(count));
+            temp_char[count] = String.valueOf(user_Str.charAt(count));
             count++;
         }
 
+        findMaxString(temp_char,dict);
+    }
 
+    private static void findMaxString(String[] temp_char, ArrayList<String> dict) {
+        int count = 0;
+        while (count != temp_char.length){
+
+        }
     }
 }
